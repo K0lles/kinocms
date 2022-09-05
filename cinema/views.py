c60616b14685760095ec110278712aff5e6fec59
K0lles <oleksijkolotilo63@gmail.com>
2022-09-05 21:38:47 +0300

    created home page

--- cinema/views.py
+++ cinema/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from banner.models import *
+from movie.models import *
+from django.utils import timezone
+from datetime import datetime
+
+
+def header_data():
+    """Returns data, which will be in the header on each user page"""
+    pass
 
 
 def home_view(request):
     background_banner = BackgroundBanner.objects.first()
+    main_top_banner_photos = MainTopBanner.objects.prefetch_related('maintopbannerphoto_set').first()
+    sessions = Session.objects.prefetch_related('movie', 'hall').filter(date__month=timezone.now().month)
 
     context = {
-        'background': background_banner.photo.url if background_banner.background == 1 else None
+        'title': 'KinoCMS | Головна',
+        'background': background_banner.photo.url if background_banner.background == 1 else None,
+        'photos': main_top_banner_photos.maintopbannerphoto_set.all(),
+        'interval': main_top_banner_photos.turning_speed * 1000,
+        'sessions': sessions,
+        'admin': True if (request.user.is_authenticated and request.user.is_superuser) else False
     }
 
     return render(request, 'cinema/home.html', context=context)
